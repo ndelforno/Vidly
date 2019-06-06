4854517aaa3382690e4691b0bc80494abe3b85ea
nicolas.delforno <nicolas.delforno@intelex.com>
2019-06-06 19:02:22 -0400

    test commit

---
+++ vidly/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+
+namespace vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { name = "Shrek" };
+            //return View(movie);
+            //return Content("hello world");
+            //return HttpNotFound();
+            return RedirectToAction("index", "home", new { page = 1, sortBy = "name" });
+
+
+        }
+    }
+}
