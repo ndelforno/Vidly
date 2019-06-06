f17a8fd68a9a5d7b47af4ddd7bc33fddedd32b13
nicolas.delforno <nicolas.delforno@intelex.com>
2019-06-06 19:43:18 -0400

    test routes

--- vidly/Controllers/MoviesController.cs
+++ vidly/Controllers/MoviesController.cs
@@ -13,12 +13,33 @@ namespace vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { name = "Shrek" };
-            //return View(movie);
+            return View(movie);
             //return Content("hello world");
             //return HttpNotFound();
-            return RedirectToAction("index", "home", new { page = 1, sortBy = "name" });
+            //return RedirectToAction("index", "home", new { page = 1, sortBy = "name" });
 
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
 
+        public  ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+           
+            if (String.IsNullOrWhiteSpace(sortBy))
+           
+                sortBy = "name";
+
+            return Content(String.Format("pageIndex={0}&sortBy{1}", pageIndex, sortBy));
+
+        }
+
+        public  ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
